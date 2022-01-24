864ad8dbbcad08687cd16dbdfdc977e84443b884
quanglnFX13841 <quanglnq0305@gmail.com>
2022-01-24 15:08:54 +0700

    lab03_1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import logo from './logo.svg';
 import { Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
-import {STAFFS} from './shared/staffs';
+import {DISHES} from './shared/dishes';
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      staffs : STAFFS,
-      cot: 3
+      dishes : DISHES,
     };
   }
-  thaydoicot(c){
-    this.setState({cot:c});
-  }
   render(){
-    console.log('Menu.props');
     return (
       <div className="App">
         <Navbar dark color="primary">
@@ -25,15 +20,8 @@ class App extends Component{
               <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
             </div>
           </Navbar>
-          <div style={{display:"inline"}}>
-          <p style={{display:"inline",margin:"10px"}}>Chọn số cột hiển thị:</p>
-          <p onClick={()=>this.thaydoicot(1)} style={{display:"inline",margin:"10px"}}>1</p>
-          <p onClick={()=>this.thaydoicot(2)} style={{display:"inline",margin:"10px"}}>2</p>
-          <p onClick={()=>this.thaydoicot(3)}style={{display:"inline",margin:"10px"}}>3</p>
-          <p onClick={()=>this.thaydoicot(6)} style={{display:"inline",margin:"10px"}}>6</p>
-          </div>
           
-          <Menu nhanvien = {this.state.staffs} socot ={this.state.cot}/>
+          <Menu dishes = {this.state.dishes}/>
       </div>
     );
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,99 +1,64 @@
 import React, {Component} from 'react';
-import {Card,CardText, CardBody, CardTitle} from 'reactstrap';
-import dateFormat from "dateformat";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
+
 class Menu extends Component {
-     constructor(props){
-         super(props);
-         this.state ={
-             selected_nhanvien:null
-         }  
-     }
-     onnvSelect(staffs) {
-        this.setState({ selected_nhanvien:staffs});
-        
-    };
-    rendernhanvien(a){
-        if(a == null){
-            return(
-                <div>
-                    <p>Bấm vào tên nhân viên để xem thông tin</p>
-                </div>
-            )
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedDish: null
         }
-        else{
+    }
+
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if (dish != null)
             return(
-                <div className='row thongtinnhavien'>
-                <div className="col-12 col-sm-10 col-md-4">
                 <Card>
-                    {/*<CardImg width="100%" src = {staffs.image} alt={staffs.name}/>*/}
+                    <CardImg top src={dish.image} alt={dish.name} />
                     <CardBody>
-                        <h6> Họ và tên: {a.name}</h6>
-                        <p>Ngày sinh: {dateFormat(a.doB, "dd/mm/yyyy")}</p>
-                        <p>Ngày vào công ty: {dateFormat(a.startDate, "dd/mm/yyyy")}</p>
-                        <p>Phòng ban: {a.department.name}</p>
-                        <p>Số ngày nghỉ còn lại:{a.annualLeave}</p>
-                        <p>Số ngày đã làm thêm:{a.overTime}</p>
-                        <CardText></CardText>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
-                </div>
-                </div>
-            )
-        }
-    };
-     render(){
-         const menu = this.props.nhanvien.map((staffs)=>{
-             console.log(this.props.socot);
-             var x;
-             if (this.props.socot ==3){
-               x  = "col-12 col-sm-5 col-md-4 menunhanvien";
-             }
-                
-            else if (this.props.socot ==1)
-                x  = "col-12 col-sm-5 col-md-12 menunhanvien";
-            else if (this.props.socot==2)
-                x  = "col-12 col-sm-5 col-md-5 menunhanvien";
-            else
-                 x  = "col-12 col-sm-5 col-md-2 menunhanvien";
-                 
-            if (this.state.selected_nhanvien!=null){
-                if(this.state.selected_nhanvien.id ==staffs.id){
-                    return(
-                        <div key={staffs.id} className= {x}>
-                         <Card onClick={()=>this.onnvSelect(staffs)}>
-                                 <CardTitle style ={{color:'red'}}>{staffs.name}</CardTitle>
-                                 {/*<CardText>{dish.description}</CardText>*/}    
-                         </Card>
-                     </div>
-                     )}
-                else{
-                    return(
-                        <div key={staffs.id} className= {x}>
-                         <Card onClick={()=>this.onnvSelect(staffs)}>
-                                 <CardTitle>{staffs.name}</CardTitle>
-                                 {/*<CardText>{dish.description}</CardText>*/}    
-                         </Card>
-                     </div>
-                     )}
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
 
-                }
-         else{
-             return( <div key={staffs.id} className= {x}>
-             <Card onClick={()=>this.onnvSelect(staffs)}>
-                     <CardTitle>{staffs.name}</CardTitle>
-                     {/*<CardText>{dish.description}</CardText>*/}    
-             </Card>
-         </div>);}
-         });
-         return (
+    render() {
+        const menu = this.props.dishes.map((dish) => {
+            return (
+              <div  className="col-12 col-md-5 m-1">
+                <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+                  <CardImg width="100%" src={dish.image} alt={dish.name} />
+                  <CardImgOverlay>
+                      <CardTitle>{dish.name}</CardTitle>
+                  </CardImgOverlay>
+                </Card>
+              </div>
+            );
+        });
+
+        return (
             <div className="container">
                 <div className="row">
-                        {menu}
+                    {menu}
                 </div>
-                    {this.rendernhanvien(this.state.selected_nhanvien)}
+                <div className="row">
+                  <div  className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.state.selectedDish)}
+                  </div>
                 </div>
-         );
-     }
+            </div>
+        );
+    }
 }
 export default Menu;
 
