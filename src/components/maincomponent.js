de8bac67846d87b03bd7bd110252a91787f8def1
quanglnFX13841 <quanglnq0305@gmail.com>
2022-02-07 16:06:05 +0700

    lab05_1

--- src/components/maincomponent.js
+++ src/components/maincomponent.js
@@ -3,6 +3,8 @@ import { Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent.js';
 
 class Main extends Component{
   constructor(props){
@@ -19,13 +21,10 @@ class Main extends Component{
   render(){
     return (
       <div className="App">
-        <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand> 
-            </div>
-          </Navbar>
+          <Header/>
           <Menu dishes = {this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
            <DishDetail dish ={this.state.dishes.filter((dish) =>dish.id=== this.state.selectedDish)[0]}/>
+           <Footer/>
       </div>
     );
 
