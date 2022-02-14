0a7689436acaf743ee63040d29fe4a29f15934c3
quanglnFX13841 <quanglnq0305@gmail.com>
2022-02-14 13:47:50 +0700

    ASM2_3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import dateFormat from "dateformat";
+import {DEPARTMENTS} from '../shared/staffs';
 
        
 //  function CommentForm ({comment}){
@@ -26,25 +27,31 @@ const DishDetail = (props) => {
         <div className="container">
            <div className="row">
                     <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to="/menu">Nhân viên</Link></BreadcrumbItem>
                         <BreadcrumbItem active>{props.staff.name}</BreadcrumbItem>
                     </Breadcrumb>
                     <div className="col-12">
-                        <h3>{props.staff.name}</h3>
                         <hr />
                     </div>                
                 </div> 
         <div className="row">
-            <div  className="col-12 col-md-4 col-lg-3 m-1">
+            <div  className="col-12 col-md-4 col-lg-3">
                 <Card>
                     <CardImg top src={props.staff.image} alt={props.staff.name} />
+                </Card>
+            </div>
+            <div  className="col-12 col-md-8 col-lg-9">
+                <Card>
                     <CardBody>
-                        <CardTitle style = {{color:"black", textAlign: "center"}}>{props.staff.name}</CardTitle>
-                        {/* <CardText>{props.dish.description}</CardText> */}
+                        <CardTitle style = {{color:"black"}}>Họ và tên: {props.staff.name}</CardTitle>
+                         <CardText>Ngày sinh: {dateFormat(props.staff.doB,'dd/mm/yyyy')} </CardText>
+                         <CardText>Ngày vào công ty:{dateFormat(props.staff.startDate,'dd/mm/yyyy')} </CardText>
+                         <CardText>Phòng ban:{props.staff.department.name} </CardText>
+                         <CardText>Số ngày nghỉ còn lại: {props.staff.annualLeave} </CardText>
+                         <CardText>Số ngày làm thêm: {props.staff.overTime} </CardText>
                     </CardBody>
                 </Card>
-                </div>
-               {/* <CommentForm comment = {props.comments}/>*/}
+            </div>
         </div>
         </div>
     );
