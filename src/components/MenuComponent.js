669f737edc8f89a22a3a8fbc3caf4c039f28c7f3
quanglnFX13841 <quanglnq0305@gmail.com>
2022-02-21 23:31:05 +0700

    AMS3_04

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Card, CardImg,CardTitle,Button,Input, Form, Row,Col, Modal, ModalHeader, ModalBody,Label,FormGroup} from 'reactstrap';
+import { Card, CardImg,CardTitle,Button,Input, Form, Row,Col, Modal, ModalHeader, ModalBody,Label,FormFeedback} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
  function RenderMenuItem ({staffs},{keyWord}) {
@@ -25,13 +25,24 @@ class Menu extends Component{
             salaryScale:1,
             startDate:'2022-01-01',
             department:'Sale',
-            annualLeave: 0,
-            overTime: 0
+            annualLeave:0,
+            overTime: 0,
+            isModalOpen: false,
+            touched: {
+                username: false,
+                salaryScale:false,
+                annualLeave:false,
+                overTime:false
+
+            }
         }
         this.handleSearch=this.handleSearch.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
         this.handleAddstaff=this.handleAddstaff.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+        this.validate = this.validate.bind(this);
+        this.addNhanvien = this.addNhanvien.bind(this);
     }
 
     handleAddstaff(event){
@@ -44,7 +55,6 @@ class Menu extends Component{
         this.setState({
             staffs:result,
             name:this.name.value,
-            isModalOpen: false,
         });
         
     }
@@ -58,15 +68,75 @@ class Menu extends Component{
             isModalOpen: !this.state.isModalOpen
           });
     }
-    render(){
-        const menu = this.state.staffs.map((staffs) => {
-            return (
-              <div  className="col-12 col-md-4 col-lg-2">
-                <RenderMenuItem staffs = {staffs} keyWord ={this.state.key} />
-              </div>
-            );
+    handleBlur = (field) => (evt) => {
+        this.setState({
+            touched: { ...this.state.touched, [field]: true }
         });
-        return(
+    }
+    validate(username, salaryScale, annualLeave, overTime) {
+        const errors = {
+            username: '',
+            salaryScale:'',
+            annualLeave:'',
+            overTime:'',
+        };
+
+        if (this.state.touched.username && username.length < 3)
+            errors.username = 'Tên ít nhất 3 kí tự';
+        else if (this.state.touched.username && username.length > 20)
+            errors.username = 'Tên tối đa 20 kí tự';
+        if (this.state.touched.salaryScale && salaryScale.length<=0)
+            errors.salaryScale = 'Bạn chưa nhập';
+        else if (this.state.touched.salaryScale && isNaN(salaryScale))
+            errors.salaryScale = 'Hệ số lương phải là một số';
+        else if (this.state.touched.salaryScale && !(parseFloat(salaryScale)>=1&& parseFloat(salaryScale)<=3))
+            errors.salaryScale = 'Hệ số lương >= 1 và <= 3';
+        if (this.state.touched.annualLeave && annualLeave.length<=0)
+            errors.annualLeave = 'Bạn chưa nhập';
+        else if (this.state.touched.annualLeave && isNaN(annualLeave))
+            errors.annualLeave = 'kiểu nhập vào phải là số';
+        else if(this.state.touched.annualLeave && parseFloat(annualLeave)<0)
+            errors.annualLeave = 'Không thể nhập vào số <0';
+        
+        if (this.state.touched.overTime && overTime.length<=0)
+            errors.overTime = 'Bạn chưa nhập';
+        else if (this.state.touched.overTime && isNaN(overTime))
+            errors.overTime = 'kiểu nhập vào phải là số';
+        else if(this.state.touched.overTime && parseFloat(overTime)<0)
+            errors.overTime = 'Không thể nhập vào số <0';
+
+        return errors;
+    }
+    addNhanvien(errors,event){
+        if (this.state.touched.username===true){
+            if(errors.username ===''&& errors.salaryScale ===''&& errors.annualLeave ===''&& 
+                errors.overTime ===''){
+                console.log('Thêm nhân viên');
+                this.setState({
+                    isModalOpen:false,
+                    touched:{ 
+                        username: false,
+                        salaryScale:false,
+                        annualLeave:false,
+                        overTime:false
+                      }
+                });
+                
+            }
+        }
+        else
+            return;
+    }
+        render(){
+            const errors= this.validate(this.state.username, this.state.salaryScale, this.state.annualLeave, this.state.overTime);
+            const menu = this.state.staffs.map((staffs) => {
+                return (
+                    <div  className="col-12 col-md-4 col-lg-2">
+                        <RenderMenuItem staffs = {staffs} keyWord ={this.state.key} />
+                    </div>
+                );
+            });
+            return(
                 <div className="container">
                     <div className="row">
                             <div className="col-9 col-md-3 col-lg-2" style={{marginTop:"10px"}} >
@@ -109,8 +179,12 @@ class Menu extends Component{
                             <Col className="col-8">
                                 <Input type="text" id="username" name="username"
                                     placeholder ="Lã Ngọc Quang"
+                                    valid={errors.username === ''}
+                                    invalid={errors.username !== ''}
+                                    onBlur={this.handleBlur('username')}
                                     value={this.state.username}
                                     onChange={this.handleInputChange} />
+                                <FormFeedback>{errors.username}</FormFeedback>
                             </Col>
                         </div>
 
@@ -131,6 +205,7 @@ class Menu extends Component{
                             <Col className="col-8">
                                 <Input type="date" id="startDate" name="startDate"
                                     value={this.state.startDate}
+                        
                                     onChange={this.handleInputChange}   />
                             </Col>
                         </div>
@@ -159,7 +234,12 @@ class Menu extends Component{
                                 <Input type="text" id="salaryScale" name="salaryScale"
                                     placeholder ="1->3"
                                     value={this.state.salaryScale}
+                                    valid={errors.salaryScale === ''}
+                                    invalid={errors.salaryScale !== ''}
+                                    onBlur={this.handleBlur('salaryScale')}
+                                    value={this.state.salaryScale}
                                     onChange={this.handleInputChange} />
+                                <FormFeedback>{errors.salaryScale}</FormFeedback>
                             </Col>
                         </div>
                         <div className="form-group row">
@@ -169,7 +249,11 @@ class Menu extends Component{
                             <Col className="col-8">
                                 <Input type="text" id="annualLeave" name="annualLeave"
                                     value={this.state.annualLeave}
+                                    valid={errors.annualLeave === ''}
+                                    invalid={errors.annualLeave !== ''}
+                                    onBlur={this.handleBlur('annualLeave')}
                                     onChange={this.handleInputChange}  />
+                                <FormFeedback>{errors.annualLeave}</FormFeedback>
                             </Col>
                         </div>
                         <div className="form-group row">
@@ -179,12 +263,17 @@ class Menu extends Component{
                             <Col className="col-8">
                                 <Input type="text" id="overTime" name="overTime"
                                     value={this.state.overTime}
+                                    valid={errors.overTime === ''}
+                                    invalid={errors.overTime !== ''}
+                                    onBlur={this.handleBlur('overTime')}
                                     onChange={this.handleInputChange}  />
+                                <FormFeedback>{errors.overTime}</FormFeedback>
                             </Col>
                         </div> 
                         <div class="form-group row">
                              <div class="col-12  text-center">
                                 <Button type="submit" color="primary"
+                                 onSubmit = {this.addNhanvien(errors)}
                                 >Thêm nhân viên</Button>
                             </div>
                         </div>  
