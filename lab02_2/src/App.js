aad9999a5ca34c273f34ed89b20aada5fa3e7934
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-06-16 16:20:51 +0700

    commit lab02_2

---
+++ lab02_2/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React,{Component} from 'react'
+import {Navbar,NavbarBrand} from 'reactstrap'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+            <Navbar dark color="primary">
+              <div className="container">
+                <NavbarBrand href='/'>
+                  Ristorance Con fusion 
+
+                </NavbarBrand>
+              </div>
+            </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
