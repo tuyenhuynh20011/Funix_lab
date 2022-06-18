bdac3035776741204539ba5f1ab2a60108f5e342
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-06-18 14:40:08 +0700

    add lab03.1

--- lab02_2/src/App.js
+++ lab02_2/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import Menu from './components/MenuComponents'
 import React,{Component} from 'react'
+import { DISHES } from './shared/dishes';
 import {Navbar,NavbarBrand} from 'reactstrap'
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dishes:DISHES
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,11 +17,10 @@ class App extends Component {
               <div className="container">
                 <NavbarBrand href='/'>
                   Ristorance Con fusion 
-
                 </NavbarBrand>
               </div>
             </Navbar>
-            <Menu />
+            <Menu dishes={this.state.dishes} />
       </div>
     );
   }
---
+++ lab02_2/src/shared/dishes.js
@@ -0,0 +1,33 @@
+export const DISHES=[
+    {
+    id: 0,
+    name:'Uthappizza',
+    image: 'https://th.bing.com/th/id/OIP.Xgz0Z2vXUT5QOz8dkX-CkgHaE3?w=278&h=182&c=7&r=0&o=5&dpr=1.25&pid=1.7',
+    category: 'mains',
+    label:'Hot',
+    price:'4.99',
+    featured: true,
+    description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
+    },
+    {
+    id: 1,
+    name:'Zucchipakoda',
+    image: 'https://th.bing.com/th/id/OIP.lEa60MoPEz7kNWZQBc_a3wHaEo?w=292&h=182&c=7&r=0&o=5&dpr=1.25&pid=1.7',
+    category: 'appetizer',
+    label:'',
+    price:'1.99',
+    featured: false,
+    description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+    },
+
+    {
+    id: 3,
+    name:'ElaiCheese Cake',
+    image: 'https://th.bing.com/th/id/OIP.Hk_8DGzWZud9hZZ35WTgnQHaEo?w=297&h=185&c=7&r=0&o=5&dpr=1.25&pid=1.7',
+    category: 'dessert',
+    label:'',
+    price:'2.99',
+    featured: false,
+    description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+    }
+]
